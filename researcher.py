b159860e36961672ff7ea76c20bd9c9535960fdd
Vincent Yip <realsharkguy@gmail.com>
2023-08-27 18:32:07 -0400

    web scraper tool for the langchain. If content of website is too long, we condense it and create a summary.

--- researcher.py
+++ researcher.py
@@ -40,3 +40,86 @@ def search(query):
     print(response.text)
 
     return response.text
+# 2. Tool for scraping
+def scrape_website(objective: str, url: str):
+    # scrape website, and also will summarize the content based on objective if the content is too large
+    # objective is the original objective & task that user give to the agent, url is the url of the website to be scraped
+
+    print("Scraping website...")
+    # Define the headers for the request
+    headers = {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+    }
+
+    # Define the data to be sent in the request
+    data = {
+        "url": url
+    }
+
+    # Convert Python object to JSON string
+    data_json = json.dumps(data)
+
+    # Send the POST request
+    post_url = f"https://chrome.browserless.io/content?token={brwoserless_api_key}"
+    response = requests.post(post_url, headers=headers, data=data_json)
+
+    # Check the response status code
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        text = soup.get_text()
+        print("CONTENTTTTTT:", text)
+
+        if len(text) > 10000:
+            output = summary(objective, text)
+            return output
+        else:
+            return text
+    else:
+        print(f"HTTP request failed with status code {response.status_code}")
+
+
+def summary(objective, content):
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
+    docs = text_splitter.create_documents([content])
+    map_prompt = """
+    Write a summary of the following text for {objective}:
+    "{text}"
+    SUMMARY:
+    """
+    map_prompt_template = PromptTemplate(
+        template=map_prompt, input_variables=["text", "objective"])
+
+    summary_chain = load_summarize_chain(
+        llm=llm,
+        chain_type='map_reduce',
+        map_prompt=map_prompt_template,
+        combine_prompt=map_prompt_template,
+        verbose=True
+    )
+
+    output = summary_chain.run(input_documents=docs, objective=objective)
+
+    return output
+
+
+class ScrapeWebsiteInput(BaseModel):
+    """Inputs for scrape_website"""
+    objective: str = Field(
+        description="The objective & task that users give to the agent")
+    url: str = Field(description="The url of the website to be scraped")
+
+
+class ScrapeWebsiteTool(BaseTool):
+    name = "scrape_website"
+    description = "useful when you need to get data from a website url, passing both url and objective to the function; DO NOT make up any url, the url should only be from the search results"
+    args_schema: Type[BaseModel] = ScrapeWebsiteInput
+
+    def _run(self, objective: str, url: str):
+        return scrape_website(objective, url)
+
+    def _arun(self, url: str):
+        raise NotImplementedError("error here")
